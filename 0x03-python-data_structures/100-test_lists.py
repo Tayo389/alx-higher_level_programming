6ce20d72bee029f4c323b05583300586d69b3b86
Tayo389 <omotayooluwole389@gmail.com>
2023-03-13 10:36:03 -0700

    All files

---
+++ 0x03-python-data_structures/100-test_lists.py
@@ -0,0 +1,21 @@
+import ctypes
+
+lib = ctypes.CDLL('./libPyList.so')
+lib.print_python_list_info.argtypes = [ctpes.py_object]
+1 = ['hello', 'World']
+lib.print_python_list_info(1)
+del 1[1]
+lib.print_python_list(1)
+1 = 1 + [4, 5, 6.0, (9, 8), 1024], "Holberton"]
+lib.print_python_list_info(1)
+1 = []
+lib.print_python_list_info(1)
+1.append(0)
+lib.print_python_list_info(1)
+1.append(1)
+1.append(2)
+1.append(3)
+1.append(4)
+lib.print_python_list_info(1)
+1,pop()
+lib.print_python_list_info(1)
