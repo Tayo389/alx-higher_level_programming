2a25353cae272bd1f965ca91853382ab21209240
Tayo389 <omotayooluwole389@gmail.com>
2023-03-14 01:54:24 -0700

    All files

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,12 +7,11 @@
  * Return: pointer to the first node in the new list
  */
 void reverse_listint(listint_t **head)
- */
- {
+{
         listint_t *prev = NULL;
         listint_t *current = *head;
         listint_t *next = NULL;
-        
+
         while (current)
         {
                 next = current->next;
@@ -22,51 +21,52 @@ void reverse_listint(listint_t **head)
         }
 
         *head = prev;
- }
+}
 
 /**
- * is_palindrome _ checks if a linked list is a palindrome
+ * is_palindrome - checks if a linked list is a palindrome
  * @head: double pointer to the linked list
  *
  * Return: 1 if it is, 0 if not
  */
-int is__palindrome(listint_t **head)
+int is_palindrome(listint_t **head)
 {
-         listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+        listint_t *slow = *head, *fast = *head, *temp =*head, *dup = NULL;
 
-         if (*head == NULL || (*head)->next == NULL)
-                 return (1);
+        if (*head == NULL || (*head)->next == NULL)
+                return (1);
 
-         while (1)
-         {
-                 fast = fast->next->next;
-                 if (!fast)
-                 {
+        while (1)
+        {
+                fast = fast->next->next;
+                if (!fast)
+                {
                          dup = slow->next;
                          break;
-                 }
-                 if (!fast->next)
-                 {
-                         dup = slow->next->next;
-                         break;
-                 }
-                 slow = slow->next;
-         }
+                }
+                if (!fast->next)
+                {
+                        dup = slow->next->next;
+                        break;
+                }
+                slow = slow->next;
+        }
 
-         reverse_listint(&dup);
+        reverse_listint(&dup);
 
-         while (dup && temp)
-         {
-                 if (temp->n == dup->n)
-                 {
-                          dup = dup->next;
-                          temp = temp->next;
-                 }
-                 else
-                          return (0);
+        while (dup && temp)
+        {
+                if (temp->n == dup->n)
+                {
+                         dup = dup->next;
+                         temp = temp->next;
+                }
+                else
+                         return (0);
          }
 
          if (!dup)
                  return (1);
 
          return (0);
+}
