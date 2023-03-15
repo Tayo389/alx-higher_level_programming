c84e52e8b799c5645e7a3c05dd7a740bfc70a2fd
Tayo389 <omotayooluwole389@gmail.com>
2023-03-15 09:58:31 -0700

    All files

--- 0x04-python-more_data_structures/9-multiple_by_2.py
+++ 0x04-python-more_data_structures/9-multiple_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def multiple_by_2(a_dictionary):
+def multiply_by_2(a_dictionary):
     """
     A function that returns a new dictionary
     with all values multiplied by 2
